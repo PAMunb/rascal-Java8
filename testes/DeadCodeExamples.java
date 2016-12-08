58d1f5db5946773a1c6830c7b32d5373339958b6
Leonardo Moreira <leohmoreira@gmail.com>
2016-12-08 00:47:02 -0200

    Mais alguns casos de teste

--- testes/DeadCodeExamples.java
+++ testes/DeadCodeExamples.java
@@ -9,9 +9,33 @@ class DeadCodeExamples {
 		int x,y,z;
 		
 		//Teste 1 - eliminando atribuicoes
+		/*
 		y = 4;
 		x = 2;
 		z = 4;
+		*/
+		
+		//Teste 2 - preservando alguma coisa
+		/*
+		y = 4;
+		x = 2;
+		z = y;
+		*/
+		
+		//Teste 3 - variavel usada como parametro
+		/*
+		y = 4;
+		x = 2;
+		z = teste(y);
+		*/
+		
+		//Teste 4 - PostfixExpression
+		
+		y = 4;
+		x = 2;
+		z = y;
+		y++;
+		x++;
 		
 	}
 }
